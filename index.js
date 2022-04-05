8e2c701df55243a9ef12bcebd75e4fea615d66b5
Julia Loretta <loretta_deli@hotmail.com>
2022-04-05 19:30:45 -0300

    attacks start

--- index.js
+++ index.js
@@ -9,16 +9,26 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, lastKey, color = 'red' }) {
         this.position = position
         this.velocity = velocity
         this.height = 150
-        this.lastKey
+        this.lastKey = lastKey
+        this.attackBox = {
+            position: this.position,
+            width:  100,
+            height: 50 
+        }
+        this.color = color
     }
 
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle =  this.color
         c.fillRect(this.position.x, this.position.y, 50, this.height)
+
+        // attack box
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
@@ -53,7 +63,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'blue'
 })
 
 console.log(player);
@@ -87,9 +98,9 @@ function animate() {
     enemy.velocity.x = 0
 
     //Player Movement
-    if (keys.a.pressed && player.lastKey === 'a') {
+    if (keys.a.pressed && lastKey === 'a') {
         player.velocity.x = -5
-    } else if (keys.d.pressed && player.lastKey === 'd') {
+    } else if (keys.d.pressed && lastKey === 'd') {
         player.velocity.x = 5
     }
     //enemy movement
@@ -97,6 +108,11 @@ function animate() {
         enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
+    }
+
+    //detect for collision
+    if(player.attackBox.position.x = player.attackBox.width >= enemy.position.x) {
+        console.log('go')
     }
 }
 
